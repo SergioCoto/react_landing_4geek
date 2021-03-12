173b11c1af44c71c7260381c5288d831fdb16eb4
SergioCoto <coto.sergio@gmail.com>
2021-03-12 00:47:42 +0000

    otro

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -7,7 +7,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 export function Home() {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Sergio!</h1>
+			<h1>Hello Sergio otro!</h1>
 			<p>
 				<img src={rigoImage} />
 			</p>
